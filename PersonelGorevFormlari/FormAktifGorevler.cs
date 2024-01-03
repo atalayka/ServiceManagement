34e2d5cc5ea47a19312a69f4afb6d1108470bb74
AT K <kabakciatalay@gmail.com>
2024-01-03 23:57:11 +0300

    aktif görevler formu

---
+++ PersonelGorevFormlari/FormAktifGorevler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Is_Takip_Proje.PersonelGorevFormlari
+{
+    public partial class FormAktifGorevler : Form
+    {
+        public FormAktifGorevler()
+        {
+            InitializeComponent();
+        }
+    }
+}
